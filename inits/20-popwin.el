9d21aa744caf8eb52ed4c9ede462d7950ff93994
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2015-06-20 11:47:06 +0900

    popwinのOFF

--- inits/20-popwin.el
+++ inits/20-popwin.el
@@ -1,11 +1,11 @@
-(require 'popwin)
-
-(setq display-buffer-function 'popwin:display-buffer)
-(setq popwin:popup-window-position 'bottom)
-(setq popwin:close-popup-window-timer-interval 0.5)
-
-;; キーマップの変更
-(global-set-key (kbd "C-x p") popwin:keymap)
+;; (require 'popwin)
+;; 
+;; (setq display-buffer-function 'popwin:display-buffer)
+;; (setq popwin:popup-window-position 'bottom)
+;; (setq popwin:close-popup-window-timer-interval 0.5)
+;; 
+;; ;; キーマップの変更
+;; (global-set-key (kbd "C-x p") popwin:keymap)
 
 ;; ポップアップ表示設定
 ;; (push '("*Occur*") popwin:special-display-config)

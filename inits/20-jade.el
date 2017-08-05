8213f49f3e1188af0c9f8c565483f15124f81234
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2017-08-05 14:17:48 +0900

    aspell & pug設定

--- inits/20-jade.el
+++ inits/20-jade.el
@@ -1,2 +1,3 @@
 (require 'jade-mode)
+(add-to-list 'auto-mode-alist '("\\.pug$"  . jade-mode))
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
--- inits/30-aspell.el
+++ inits/30-aspell.el
@@ -4,3 +4,12 @@
 
 (eval-after-load "ispell"
   '(add-to-list 'ispell-skip-region-alist '("[^\000-\377]+")))
+
+(defun ispell-get-word (following)
+  (when following (camelCase-forward-word 1))
+  (let* ((start (progn (camelCase-backward-word 1)
+                       (point)))
+         (end   (progn (camelCase-forward-word 1)
+                       (point))))
+    (list (buffer-substring-no-properties start end)
+          start end)))

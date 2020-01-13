baf8dc1c6f676495ee56cc6bdb00b3ac6dbe33a6
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2020-01-13 13:31:28 +0900

    Add typescript-mode

--- init.el
+++ init.el
@@ -22,6 +22,7 @@
     bazel-mode
     scss-mode
     scala-mode
+    typescript-mode
     web-mode
     ;; apache-mode
     ;; google-c-style
---
+++ inits/20-typescript.el
@@ -0,0 +1,7 @@
+(require 'typescript-mode)
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+
+(add-hook 'typescript-mode-hook
+  (lambda ()
+    (setq typescript-indent-level 2)
+  ))

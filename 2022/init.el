05328f992e0c43da8a0fb54b7c241e23a73f928a
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2022-01-30 14:14:08 +0900

    update color theme.

--- 2022/init.el
+++ 2022/init.el
@@ -2,6 +2,33 @@
 ;; this enables this running method
 ;;   emacs -q -l ~/.debug.emacs.d/init.el
 
+(custom-set-variables `(gc-cons-threshold ,(* gc-cons-threshold 20)))
+
+(setq user-init-file (or load-file-name (buffer-file-name)))
+(setq user-emacs-directory (file-name-directory user-init-file))
+
+;; tmux 内にいる && Emacs がインタラクティブに起動 && "Emacs" が window-name にないとき
+;; window-name を設定する。
+;; また、Emacs 終了時に automatic-rename を有効にする
+(let ((case-fold-search nil)            ; case-sensitive
+      (tmux-title-of-emacs "Emacs"))
+  (when (and (getenv "TMUX")
+             (not noninteractive)
+             (not (string-match-p
+                   (concat  "^[0-9]+: " tmux-title-of-emacs)
+                   (shell-command-to-string "tmux lsw"))))
+    (shell-command (format "tmux rename-window '%s'" tmux-title-of-emacs))
+    (add-hook 'kill-emacs-hook
+              (lambda ()
+                (shell-command "tmux set-window-option automatic-rename on")))))
+
+(define-key key-translation-map (kbd "C-h") (kbd "<DEL>"))
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(setq garbage-collection-messages t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; this enables this running method
+;;   emacs -q -l ~/.debug.emacs.d/{{pkg}}/init.el
 (eval-and-compile
   (when (or load-file-name byte-compile-current-file)
     (setq user-emacs-directory
@@ -10,9 +37,9 @@
 
 (eval-and-compile
   (customize-set-variable
-   'package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
+   'package-archives '(("org"   . "https://orgmode.org/elpa/")
                        ("melpa" . "https://melpa.org/packages/")
-                       ("org"   . "https://orgmode.org/elpa/")))
+                       ("gnu"   . "https://elpa.gnu.org/packages/")))
   (package-initialize)
   (unless (package-installed-p 'leaf)
     (package-refresh-contents)
@@ -30,6 +57,7 @@
     ;; initialize leaf-keywords.el
     (leaf-keywords-init)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; 見た目
 (leaf *font
   :config
@@ -37,24 +65,104 @@
   (set-fontset-font t 'unicode (font-spec :name "HackGenNerdConsole") nil 'append)
   (set-fontset-font t '(#x1F000 . #x1FAFF) (font-spec :name "Noto Color Emoji") nil 'append))
 
-;; シンタックスハイライトをグローバルで有効化
+(leaf basic-key-bindings
+  :init
+  (global-unset-key (kbd "C-q"))
+  (global-unset-key (kbd "C-x DEL"))
+  (leaf basic-key-bindings-bind
+    :init
+    (defun switch-to-used-buffer ()
+      (interactive)
+      (switch-to-buffer nil))
+
+    :bind (("<tab>" . indent-for-tab-command)
+           
+           ("C-x <RET> u" . revert-buffer-with-coding-system-utf-8-unix)
+           ("C-x <RET> s" . revert-buffer-with-coding-system-japanese-cp932-dos))))
+
+(leaf window-system
+  :if (window-system)
+  :init
+  (global-unset-key (kbd "C-t"))
+  (set-face-foreground 'vertical-border "#555")
+  (defun my-emacs-startup-hook-handler ()
+    (toggle-frame-fullscreen)
+    (scroll-bar-mode -1)
+    (tool-bar-mode -1)
+    (menu-bar-mode 1)
+    (global-hl-line-mode 1))
+  (leaf all-the-icons
+    :doc "A library for inserting Developer icons"
+    :req "emacs-24.3"
+    :tag "lisp" "convenient" "emacs>=24.3"
+    :added "2021-04-08"
+    :url "https://github.com/domtronn/all-the-icons.el"
+    :emacs>= 24.3
+    :ensure t
+    :require t)
+  (leaf doom-modeline
+    :doc "A minimal and modern mode-line"
+    :req "emacs-25.1" "all-the-icons-2.2.0" "shrink-path-0.2.0" "dash-2.11.0"
+    :tag "mode-line" "faces" "emacs>=25.1"
+    :added "2021-04-08"
+    :url "https://github.com/seagle0128/doom-modeline"
+    :emacs>= 25.1
+    :ensure t
+    :after all-the-icons
+    :init (doom-modeline-mode 1))
+  (leaf pos-tip
+    :doc "Show tooltip at point"
+    :tag "tooltip"
+    :added "2021-04-12"
+    :ensure t
+    :custom ((pos-tip-border-width . 2)
+             (pos-tip-internal-border-width . 5)
+             (pos-tip-foreground-color . "gray20")
+             (pos-tip-background-color . "light cyan"))))
+
+(leaf themes
+  :custom-face
+  (mode-line          . '((t (:box "dark olive green"))))
+  (mode-line-inactive . '((t (:box "#445"))))
+  :config
+  (leaf color-theme-sanityinc-solarized
+    :disabled t
+    :doc "A version of Ethan Schoonover's Solarized themes"
+    :req "emacs-24.1" "cl-lib-0.6"
+    :tag "themes" "faces" "emacs>=24.1"
+    :added "2021-03-19"
+    :url "https://github.com/purcell/color-theme-sanityinc-solarized"
+    :emacs>= 24.1
+    :ensure t
+    :config (load-theme 'sanityinc-solarized-dark t))
+  (leaf doom-themes
+    :doc "an opinionated pack of modern color-themes"
+    :req "emacs-25.1" "cl-lib-0.5"
+    :tag "nova" "faces" "icons" "neotree" "theme" "one" "atom" "blue" "light" "dark" "emacs>=25.1"
+    :added "2021-04-09"
+    :url "https://github.com/hlissner/emacs-doom-theme"
+    :emacs>= 25.1
+    :ensure t
+    :config
+    (load-theme 'doom-dark+ t)
+    (custom-set-faces '(default ((t (:background "#030303")))))
+    ))
+
+(leaf ansi-color
+  :config
+  (defun my-colorize-compilation-buffer ()
+    (when (eq major-mode 'compilation-mode)
+      (ansi-color-apply-on-region compilation-filter-start (point-max))))
+  :hook (compilation-filter-hook . my-colorize-compilation-buffer))
+
+
 (leaf font-core :config (global-font-lock-mode 1))
-
-(leaf *global-set-key
-  :leaf-autoload nil
-  :bind
-  ("<tab>" . indent-for-tab-command)
-
-  ("C-x <RET> u" . revert-buffer-with-coding-system-utf-8-unix)
-  ("C-x <RET> s" . revert-buffer-with-coding-system-japanese-cp932-dos)
-
-  )
-
 (leaf dired
   :custom
   (dired-isearch-filenames . t)
   (dired-listing-switches  . "-al --group-directories-first"))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; 独立した関数定義
 (leaf cus-edit
   :doc "tools for customizing Emacs and Lisp packages"
@@ -189,6 +297,17 @@
   :custom ((ivy-prescient-retain-classic-highlighting . t))
   :global-minor-mode t)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Proguram Languages
+;;; Scala
+
+(leaf scala-mode
+  :ensure t)
+  ;; :hook
+  ;; (scala-mode-hook . lsp)
+  ;; (scala-mode-hook . lsp-format-before-save))
+  ;; :config
+  ;; (leaf lsp-metals :ensure t :require t)
+  ;; (leaf smartparens :config (sp-local-pair 'scala-mode "{" nil :post-handlers nil)))
 
 (provide 'init)
-;;; init.el ends here

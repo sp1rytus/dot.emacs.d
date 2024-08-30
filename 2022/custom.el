9d9a12a6f0e57b24378bc895a8bbae8fe0cec0c3
Yoshinobu Kinugasa <yoshinobu.kinugasa@ixias.net>
2024-08-30 21:05:57 +0900

    updated

--- 2022/custom.el
+++ 2022/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(dockerfile-mode web-mode yaml-mode json-mode mhtml-mode typescript-mode php-mode scala-mode whitespace company-c-headers company yasnippet-snippets yasnippet flyspell doom-themes macrostep leaf-tree leaf-convert blackout el-get hydra leaf-keywords leaf)))
+   '(dockerfile-mode terraform-mode yaml-mode json-mode web-mode typescript-mode php-mode scala-mode whitespace company-c-headers company yasnippet-snippets yasnippet flyspell doom-themes macrostep leaf-tree leaf-convert blackout el-get hydra leaf-keywords leaf)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

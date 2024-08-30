364bb2785eb6dc2a18fd13a427fa856bad21548b
Yoshinobu Kinugasa <yoshinobu.kinugasa@ixias.net>
2024-08-30 22:54:28 +0900

    reverted

--- 2024/inits/01-locale.el
+++
@@ -1,5 +0,0 @@
-(set-language-environment   "Japanese")
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(setq buffer-file-coding-system 'utf-8)
